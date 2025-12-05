908523c432faf849dddd201855c9a6bbf4fd759d
mad4x <franoemi07@gmail.com>
2025-12-05 14:32:32 +0100

    layout dashboard

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
 import React, {ReactNode} from 'react'
+import Navbar from "@/components/navigation/navbar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <p>Root Layout</p>
+      <Navbar />
       {children}
     </>
   )
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import React from 'react'
+import {Button} from "@/components/ui/button";
+import {redirect} from "next/navigation";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col shadow-2xl border gap-5 py-6 px-8 rounded-2xl items-center">
+        <h1 className="text-2xl font-bold">Benvenuto in Fatbook</h1>
+        <Link href="/dashboard" className="justify-center items-center">
+          <Button className="text-lg px-8 py-5">Entra</Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+export default Home
