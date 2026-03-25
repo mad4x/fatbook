69c3f89d6b818e92374cdcfb16748fb892c84f3c
mad4x <franoemi07@gmail.com>
2026-03-25 23:03:37 +0100

    aggiunte sezioni al pannello vicepreside

--- frontend/constants/index.tsx
+++ frontend/constants/index.tsx
@@ -1,4 +1,4 @@
-import {Users} from "lucide-react";
+import {ArrowLeftRight, ClipboardPen, Files, Users} from "lucide-react";
 
 export const SIDEBAR_ELEMENTS = [
   {
@@ -29,6 +29,24 @@ export const PANNELLO_VICEPRESIDE_ELEMENTS = [
         "href": "/vicepresidenza/docenti",
         "title": "Gestione Docenti",
         "description": "Aggiungi o rimuovi professori",
+        "icon": <Users size={32} />
+    },
+    {
+        "href": "/vicepresidenza/avvisi",
+        "title": "Gestione avvisi",
+        "description": "Crea avvisi e gestisci le bozze",
+        "icon": <Files size={32} />
+    },
+    {
+        "href": "/vicepresidenza/sostituzioni",
+        "title": "Controlla sostituzioni",
+        "description": "Sostituisci i docenti assenti",
+        "icon": <ArrowLeftRight size={32} />
+    },
+    {
+        "href": "/vicepresidenza/assenze",
+        "title": "Storico assenze",
+        "description": "Traccia e gestisci le assenze",
         "icon": <Users size={32} />
     }
 ]
